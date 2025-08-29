bde91b16fbe7f767852b936947a8285f6b9d9ef9
Vlad <149190278+kicker405@users.noreply.github.com>
2025-08-29 14:12:24 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,21 @@
-const { Pool } = require('pg');
+const knex = require('knex');
 require('dotenv').config();
 
-const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+const DB = knex({
+  client: 'pg',
+  connection: {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  }
 });
 
-module.exports = pool;
+// Проверка подключения
+DB.raw('SELECT 1')
+  .then(() => console.log('✅ Connected to PostgreSQL with Knex'))
+  .catch(err => console.error('❌ Database connection error:', err));
+
+module.exports = DB;
