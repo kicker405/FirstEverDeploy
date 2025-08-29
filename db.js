8bf7840720bd689abdac9a1e3113c0fe5ca6cc43
Vlad <149190278+kicker405@users.noreply.github.com>
2025-08-29 13:39:12 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,57 @@
-const knex = require('knex')({
-  client: 'pg',
-  connection: process.env.DATABASE_URL + '?ssl=no-verify'
+// This code for local
+import mysql from "mysql2"
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const con = mysql.createConnection({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE
+
+})
+
+con.connect(function(error){
+    if(error){
+        return console.log("Connection error");
+
+    }else{
+        return console.log("Coonected");
+
+    }
+})
+export default con;
+
+
+// This code for server
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const con = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 
-module.exports = knex;
+con.connect((error) => {
+  if (error) {
+    console.log("❌ Connection error:", error);
+  } else {
+    console.log("✅ Connected to the MySQL database!");
+  }
+});
+
+con.query('SHOW TABLES', (err, results) => {
+  if (err) {
+    console.log('❌ Error:', err);
+  } else {
+    console.log('✅ Tables:', results);
+  }
+});
+
+export default con;
